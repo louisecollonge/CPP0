4688649845de58da3b1716ec14b2a124fddeef3c
Louise Collonge <lcollong@c2r12p6.42lehavre.fr>
2025-04-05 12:02:21 +0200

    finished ex02 + README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# CPP0
+## CPP module 00
+
+# Description
+
+Ce module est le premier d’une série de modules visant à apprendre la programmation orientée objet en C++. Il introduit les bases du langage C++, en particulier les différences fondamentales entre C et C++, notamment :
+- Syntaxe de base du C++
+- Différences entre std::cout, std::cin, std::endl et les fonctions C classiques comme printf
+- Les espaces de noms (namespace)
+- Les fonctions membres et non membres
+- La manipulation des classes et des objets
+- Les constructeurs et destructeurs
+
+# Setup
+
+Clone the repository:
+```c++
+git clone git@github.com:louisecollonge/CPP0.git yourRepo && cd yourRepo
+```
+
+Install dependencies:
+```c++
+make
+```
+# Usage
+
+Run the executable:
+- ex00:
+```c++
+./megaphone
+```
+- ex01:
+```c++
+./myAwesomePhoneBook
+```
+- ex02: you need the annexes provided to students by the 42 school to run this one.
+```c++
+./Accounting
+```
--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,3 +1,6 @@
+#define BLUE	"\033[94m"
+#define RESET	"\033[0m"
+
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
@@ -11,6 +14,7 @@ int	Account::getNbAccounts( void ) { return (_nbAccounts); }
 int	Account::getTotalAmount( void ) { return (_totalAmount); }
 int	Account::getNbDeposits( void ) { return (_totalNbDeposits); }
 int	Account::getNbWithdrawals( void ) { return (_totalNbDeposits);}
+int	Account::checkAmount( void ) const { return (_amount); } // methode constante = ne peut rien modifier dans l'objet
 
 Account::Account( int initial_deposit ) : _accountIndex(_nbAccounts), _amount(initial_deposit),
 	_nbDeposits(0), _nbWithdrawals(0)
@@ -18,58 +22,58 @@ Account::Account( int initial_deposit ) : _accountIndex(_nbAccounts), _amount(in
 	_nbAccounts++;
 	_totalAmount += _amount;
 	_displayTimestamp();
-	std::cout << " index:" << _accountIndex
-			  << ";amout:" << _totalAmount
+	std::cout << " index:" << BLUE << _accountIndex << RESET
+			  << ";amount:" << BLUE << _amount << RESET
 			  << ";created" << std::endl;
 }
 
 Account::~Account( void )
 {
 	_displayTimestamp();
-	std::cout << " index:" << _accountIndex
-			  << ";amout:" << _totalAmount
+	std::cout << " index:" << BLUE << _accountIndex << RESET
+			  << ";amount:" << BLUE << _amount << RESET
 			  << ";closed" << std::endl;
 }
 
 void	Account::displayAccountsInfos( void )
 {
 	_displayTimestamp();
-	std::cout << " accounts:" << _nbAccounts
-			  << ";total:" << _totalAmount
-			  << ";deposits:" << _totalNbDeposits
-			  << ";withdrawals:" << _totalNbWithdrawals
+	std::cout << " accounts:" << BLUE << _nbAccounts << RESET
+			  << ";total:" << BLUE << _totalAmount << RESET
+			  << ";deposits:" << BLUE << _totalNbDeposits << RESET
+			  << ";withdrawals:" << BLUE << _totalNbWithdrawals << RESET
 			  << std::endl;
 }
 
 void	Account::makeDeposit( int deposit )
 {
 	_displayTimestamp();
-	std::cout << " index:" << _accountIndex
-			  << ";p_amout:" << _amount
-			  << ";deposit:" << deposit;
+	std::cout << " index:" << BLUE << _accountIndex << RESET
+			  << ";p_amount:" << BLUE << _amount << RESET
+			  << ";deposit:" << BLUE << deposit << RESET;
 	_amount += deposit;
 	_nbDeposits++;
 	_totalAmount += deposit;
 	_totalNbDeposits++;
-	std::cout << ";amount:" << _amount
-			  << ";nb_deposits:" << _nbDeposits
+	std::cout << ";amount:" << BLUE << _amount << RESET
+			  << ";nb_deposits:" << BLUE << _nbDeposits << RESET
 			  << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
 	_displayTimestamp();
-	std::cout << " index:" << _accountIndex
-			  << ";p_amout:" << _amount;
+	std::cout << " index:" << BLUE << _accountIndex << RESET
+			  << ";p_amount:" << BLUE << _amount << RESET;
 	if (withdrawal <= _amount)
 	{
 		_amount -= withdrawal;
 		_nbWithdrawals++;
 		_totalAmount -= withdrawal;
 		_totalNbWithdrawals++;
-		std::cout << ";withdrawal:" << withdrawal
-				  << ";amount:" << _amount
-				  << ";nb_withdrawals:" << _nbWithdrawals
+		std::cout << ";withdrawal:" << BLUE << withdrawal << RESET
+				  << ";amount:" << BLUE << _amount << RESET
+				  << ";nb_withdrawals:" << BLUE << _nbWithdrawals << RESET
 				  << std::endl;
 		return (true);
 	}
@@ -83,19 +87,27 @@ bool	Account::makeWithdrawal( int withdrawal )
 void	Account::displayStatus( void ) const
 {
 	_displayTimestamp();
-	std::cout << " index:" << _accountIndex
-			  << ";amount:" << _amount
-			  << ";deposits:" << _nbDeposits
-			  << ";withdrawals:" << _nbWithdrawals
+	std::cout << " index:" << BLUE << _accountIndex << RESET
+			  << ";amount:" << BLUE << _amount << RESET
+			  << ";deposits:" << BLUE << _nbDeposits << RESET
+			  << ";withdrawals:" << BLUE << _nbWithdrawals << RESET
 			  << std::endl;
 }
 
 void	Account::_displayTimestamp( void )
 {
+	time_t		*ptr = NULL;
+	std::time_t	now = std::time(ptr); // nb secondes depuis 1/1/1970
+	std::tm*	localTime = std::localtime(&now); // conversion en annee, mois, jour, h, min, s
 
-}
-
-int		Account::checkAmount( void ) const
-{
+	std::cout << "["
+			  << (localTime -> tm_year + 1900)
+			  << (localTime -> tm_mon + 1 < 10 ? "0" : "") << (localTime -> tm_mon + 1)
+			  << (localTime -> tm_mday < 10 ? "0" : "") << (localTime -> tm_mday)
+			  << "_"
+			  << (localTime -> tm_hour)
+			  << (localTime -> tm_min)
+			  << (localTime -> tm_sec)
+			  << "]";
 
 }
